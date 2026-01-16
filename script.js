355a9d32787d96215cb58559784b408ebf39be8f
Pyae Phyoe H <pyaephyoeh9@gmail.com>
2026-01-16 10:14:35 +0630

    modified script.js-v1

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js v1
+// Basic JavaScript interaction
+
+function showMessage() {
+    const msg = document.getElementById("msg");
+    msg.textContent = "Hello! JavaScript is working (v1)";
+}
